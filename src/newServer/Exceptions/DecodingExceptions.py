ce3265b6c66c7c2f3eed05b3d901fdcb525ce8de
Flintpop <tanguy.legoff2@gmail.com>
2023-11-22 14:06:22 +0100

    Beginning of refactoring using design patterns and OOP. SSH Commands class created. It contains the basics for ssh commands.

---
+++ src/newServer/Exceptions/DecodingExceptions.py
@@ -0,0 +1,2 @@
+class DecodingError(Exception):
+    pass
--- src/server/ssh/ssh_connect.py
+++ src/server/ssh/ssh_connect.py
@@ -4,17 +4,6 @@ import chardet
 from src.server.Exceptions.DecodingExceptions import DecodingError
 
 
-def execute_and_get_all_decoded_streams_ssh(ssh: paramiko.SSHClient, command: str) -> tuple[str, str, str]:
-    """
-    Execute a command and get the output, input and error streams properly decoded.
-    :param ssh: The SSH session.
-    :param command: The command to execute.
-    :return: The output, input and error streams properly decoded.
-    """
-    stdin, stdout, stderr = ssh.exec_command(command)
-    return decode_all_ssh_streams(stdin, stdout, stderr)
-
-
 def decode_stream(stream) -> str | None:
     """
     Decode a stream.
@@ -33,30 +22,3 @@ def decode_stream(stream) -> str | None:
             pass
 
     raise DecodingError("None of the tested encodings were able to decode the stream")
-
-
-def decode_all_ssh_streams(stdin, stdout, stderr) -> tuple[str, str, str]:
-    """
-    Get the output of a command.
-    :param stdin: The input stream.
-    :param stdout: The output stream.
-    :param stderr: The error stream.
-    :return: The output of the command.
-    """
-    ssh_output = stdout.read()
-    ssh_input = stdin.read()
-    ssh_errors = stderr.read()
-    detected_encoding = chardet.detect(ssh_output)['encoding']
-    return ssh_input.decode(detected_encoding), ssh_output.decode(detected_encoding), ssh_errors.decode(
-        detected_encoding)
-
-
-def get_stdout_decoded_str(ssh: paramiko.SSHClient, command: str) -> str:
-    """
-    Get the output of a command.
-    :param ssh: The SSH session.
-    :param command: The command to execute.
-    :return: The output of the command.
-    """
-    _, stdout, _ = ssh.exec_command(command)
-    return decode_stream(stdout.read())
